a5d8aded7e3d1888b1cbe30020bc2b05bc210ca0
Gulshanth <gulshanthkumar2005@gmail.com>
2025-01-05 21:12:51 +0530

    url is changed
    url is now directly connected to the server

--- myDjangoProj/myDjangoProj/urls.py
+++ myDjangoProj/myDjangoProj/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from polls import views  # Replace 'your_app_name' with the actual name of your app
+from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   path('start_page',views.startpage,name= 'startpage'),
+    path('',include("polls.urls"))
 ]
--- myDjangoProj/polls/urls.py
+++ myDjangoProj/polls/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path('start_page',views.startpage,name= 'startpage'),
+    path('',views.startpage,name= 'startpage'),
 ]
