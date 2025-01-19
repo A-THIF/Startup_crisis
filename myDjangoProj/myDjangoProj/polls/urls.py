3bf18a9b8fd68901734caa8144ec35a58c26feff
A-THIF <149256653+A-THIF@users.noreply.github.com>
2025-01-19 23:15:09 +0530

    updated the about.html
    updated the hari and ghulshath linkedin

---
+++ myDjangoProj/myDjangoProj/polls/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+urlpatterns = [
+    path('',views.startpage,name= 'startpage'),
+    path('contact/', views.contact_view, name='contact'),
+    path('selection/', views.selection, name='selection'), 
+    path('about/', views.about, name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('settings/', views.settings, name='settings'),
+]
