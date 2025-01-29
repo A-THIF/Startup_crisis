93f39e54a55e8110951e4eabbb846f1d9422724b
Gulshanth <gulshanthkumar2005@gmail.com>
2025-01-29 19:36:24 +0530

    gameflow updated cleared unlink issues

--- myDjangoProj/polls/urls.py
+++ myDjangoProj/polls/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
 from . import views
+
 urlpatterns = [
-    path('',views.startpage,name= 'startpage'),
+    path('', views.startpage, name='startpage'),
     path('contact/', views.contact_view, name='contact'),
     path('selection/', views.selection, name='selection'), 
     path('about/', views.about, name='about'),
-    path('contact/',views.contact,name='contact'),
+    path('contact/', views.contact, name='contact'),
     path('settings/', views.settings, name='settings'),
-    path('selection/gameflow.html', views.gameflow, name='gameflow'),
-    path('signout/',views.signout, name='signout'),
-    
+    path('gameflow/', views.gameflow, name='gameflow'),
+    path('signout/', views.signout, name='signout'),
 ]
